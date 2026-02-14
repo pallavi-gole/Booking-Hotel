f9986f6394c0c58ab5bdf2fdcdb9c728d9cdebe8
Pallavi Gole <pallavigole2001@gmai.com>
2026-02-14 20:21:35 +0530

    test stripe with direct url

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -246,8 +246,9 @@ export const stripePayment = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `${process.env.FRONTEND_URL}/loader/my-bookings`,
-      cancel_url: `${process.env.FRONTEND_URL}/my-bookings`,
+      success_url: `https://booking-hotel-7.onrender.com/loader/my-bookings`,
+cancel_url: `https://booking-hotel-7.onrender.com/my-bookings`,
+
       metadata: {
         bookingId,
       },
