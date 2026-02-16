8bd427317df5221b3a3b3212ca78e49977890729
Pallavi Gole <pallavigole2001@gmai.com>
2026-02-16 11:19:24 +0530

    change url

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -246,7 +246,7 @@ export const stripePayment = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `https://booking-hotel-7.onrender.com/my-bookings`,
+      success_url: `https://booking-hotel-7.onrender.com/my-bookings?success=true`,
 cancel_url: `https://booking-hotel-7.onrender.com/my-bookings`,
 
       metadata: {
